3f2cba61001477402e027606d662891553802b87
Vikkitharan Gnanasambandapillai <vikki.gsam@seeingmachines.com>
2023-09-08 17:03:44 +1000

    Add one snippets plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,6 +60,8 @@ require('lazy').setup({
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
 
+  -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
     },
   },
 
