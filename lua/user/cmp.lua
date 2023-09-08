7a74b0c4bd227a7af6cded24ffd70b2d6f936af7
Vikkitharan Gnanasambandapillai <vikki.gsam@seeingmachines.com>
2023-09-08 17:10:32 +1000

    Add more plug in in complete source and manue

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -79,6 +79,7 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
+        nvim_lua = "[NVIM_LUA]",
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
@@ -88,8 +89,9 @@ cmp.setup {
   },
 
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
   },
