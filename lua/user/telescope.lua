9286ce23d962d5c51e1d0b89e51afac60869816b
Vikkitharan Gnanasambandapillai <vikki.gsam@seeingmachines.com>
2023-10-03 10:48:06 +1100

    Replace double leader key to telescope buffers
    commit aa57eceb6bb632ff7bb8e85869254097fd21467d
    Author: Vikkitharan Gnanasambandapillai <vikki.gsam@seeingmachines.com>
    Date:   Tue Oct 3 10:47:08 2023 +1100
    
        Replace double leader key to telescope buffers

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -112,7 +112,7 @@ pickers = {
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>bf', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -127,4 +127,3 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-
