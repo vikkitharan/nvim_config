9014fe2de0fcef63bbec100a8e2f48073f62f993
Vikki <vikki.gsam@gmail.com>
2024-08-12 07:06:54 +1000

    Add more languages for treesitter parser

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,7 +3,7 @@ require("nvim-treesitter.install").prefer_git = true
 
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'go', 'lua', 'luadoc', 'markdown', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
