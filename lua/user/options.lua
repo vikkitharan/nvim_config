3a1056f5b0fa2a1924aec9f7d42323363b072655
Ravi G Ravitharan <ravitharan@gmail.com>
2025-10-18 17:32:54 +0530

    Change sw & ts to 4

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -28,8 +28,8 @@ vim.o.termguicolors = true -- NOTE: You should make sure your terminal supports 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 vim.o.infercase = true -- Adjust case for auto complete
