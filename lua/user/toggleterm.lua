c5213617e5241f821844a0ede5c7fe0af1ec0b33
Vikki <vikki.gsam@gmail.com>
2026-04-16 01:31:23 +1000

    toggleterm: modernise terminal configuration
    - Replace vim.api.nvim_buf_set_keymap with vim.keymap.set
    - Replace vim.cmd autocmd with vim.api.nvim_create_autocmd
    - Replace global _G functions with local variables and keymaps
    - Add keymaps for lazygit, node, ncdu, htop, python terminals

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -26,45 +26,32 @@ toggleterm.setup({
 	},
 })
 
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+local function set_terminal_keymaps()
+  local opts = { noremap = true, buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*toggleterm*",
+  callback = function()
+    set_terminal_keymaps()
+  end,
+})
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
-end
-
 local node = Terminal:new({ cmd = "node", hidden = true })
-
-function _NODE_TOGGLE()
-	node:toggle()
-end
-
 local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-
-function _NCDU_TOGGLE()
-	ncdu:toggle()
-end
-
 local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-	htop:toggle()
-end
-
 local python = Terminal:new({ cmd = "python", hidden = true })
 
-function _PYTHON_TOGGLE()
-	python:toggle()
-end
+vim.keymap.set('n', '<leader>tg', function() lazygit:toggle() end, { desc = 'Toggle Lazygit' })
+vim.keymap.set('n', '<leader>tn', function() node:toggle() end, { desc = 'Toggle Node' })
+vim.keymap.set('n', '<leader>tu', function() ncdu:toggle() end, { desc = 'Toggle Ncdu' })
+vim.keymap.set('n', '<leader>th', function() htop:toggle() end, { desc = 'Toggle Htop' })
+vim.keymap.set('n', '<leader>tp', function() python:toggle() end, { desc = 'Toggle Python' })
