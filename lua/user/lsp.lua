4dd8f164dfc8ed330a04fba1f763c545e3cd609a
Vikki <vikki.gsam@gmail.com>
2026-04-18 17:53:48 +1000

    lsp: enable format on save for modified lines
    - Enable lsp-format-modifications format_on_save
    - Formats only modified lines on save using clangd and .clang-format
    - Safe for shared codebases as untouched lines are not reformatted

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -77,7 +77,7 @@ local on_attach = function(client, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 
   local lsp_format_modifications = require "lsp-format-modifications"
-  lsp_format_modifications.attach(client, bufnr, { format_on_save = false })
+  lsp_format_modifications.attach(client, bufnr, { format_on_save = true })
 
   vim.keymap.set('n', '<leader>ti', function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
